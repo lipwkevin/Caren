05fb6f5085fddcc298ceaa69349d3d680bb9b179
Kevin Li <lipwkevin@gmail.com>
2017-01-25 16:46:59 -0800

    login and logout

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,24 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by_uid params[:uid].downcase
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to root_path, notice: 'Welcome'
+    else
+      flash.now[:alert] = 'Fail to log in, please try again'
+      render :new
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    # equivalent to deleting the user_id key from session
+    redirect_to root_path, notice: 'Goodbye'
+  end
+
+  def home
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :users, only: [:create, :new]
+  resources :sessions, only: [:create, :new] do
+    delete :destroy, on: :collection
+  end
+
+  root 'sessions#home'
 end
