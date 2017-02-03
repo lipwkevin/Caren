5604ae22b9fc2188d7a06f08c31bfc0e3822692c
Kevin Li <lipwkevin@gmail.com>
2017-02-02 17:44:15 -0800

    add category

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+categorys = ["Medicine","Appointment","Reminder"]
+categorys.each do |category|
+  Category.create(name:category)
+end
