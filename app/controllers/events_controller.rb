c7996a9c25a7a1a3158b1ecb1f84aef423b87a7c
Kevin Li <lipwkevin@gmail.com>
2017-02-03 17:03:11 -0800

    implement add event, need to fix to reloading page

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,4 +3,15 @@ class EventsController < ApplicationController
   def new
     @event = Event.new
   end
+
+  def create
+    event_params = params.require(:event).permit(:name,:date,:time,:category,:remarks)
+    @event = Event.new event_params
+    @event.user = current_user
+    if @event.save
+      redirect_to calendar_show_path, notice: 'Event Added'
+    else
+      render 'schedules/show'
+    end
+  end
 end
