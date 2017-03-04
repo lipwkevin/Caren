33611c6d2e52380e8ad67de3719f6a168a5ee703
Kevin Li <lipwkevin@gmail.com>
2017-03-03 18:21:59 -0800

    add time indicator color to schedule

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -1,6 +1,7 @@
 class SchedulesController < ApplicationController
   def show
     @schedule = current_user.schedules.first
+    # byebug
     @task = Task.new
   end
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,15 @@
 class Task < ApplicationRecord
    attr_accessor :everyday
   belongs_to :schedule
+
+  def get_color
+    t = time.hour
+    if (MORNING_START..NOON_START).cover? t
+      return "morning"
+    elsif (NOON_START..NIGHT_START).cover? t
+      return "noon"
+    else
+      return 'night'
+    end
+  end
 end
