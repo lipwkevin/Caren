71d7573661898b8fb8b160e9f1b0b339c28ff86b
Kevin Li <lipwkevin@gmail.com>
2017-03-03 18:24:18 -0800

    fix calender ordering

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,6 @@ class User < ApplicationRecord
   end
 
   def get_schedule(date)
-    return events.order(:time).where(date:date.to_date)
+    return events.where(date:date.to_date).order(:time)
   end
 end
