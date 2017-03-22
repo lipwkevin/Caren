589d9231b38c470c1e4a3e7f85a83cafffc81d17
Kevin Li <lipwkevin@gmail.com>
2017-03-22 15:47:40 -0700

    update: display result with js

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -2,16 +2,15 @@ class SchedulesController < ApplicationController
   before_action :authenticate_user
   def show
     @schedule = current_user.schedules.first
-    # byebug
     @task = Task.new
   end
 
   def update
-    schedule = current_user.schedules.first
-    if schedule.update params.require(:schedule).permit([:duration])
-      redirect_to schedule_show_path, notice: "Duration Updated"
+    @schedule = current_user.schedules.first
+    if @schedule.update params.require(:schedule).permit([:duration])
+      flash.now[:notice] = "Duration Updated"
     else
-      redirect_to schedule_show_path, alert: "failed to update"
+      flash.now[:alert] =  "failed to update"
     end
   end
 end
