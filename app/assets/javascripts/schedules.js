20e6e5cfbe18fae18fe53b50d553149920d3b439
Kevin Li <lipwkevin@gmail.com>
2017-04-13 22:53:38 -0700

    now able to merge cell with same text

---
+++ app/assets/javascripts/schedules.js
@@ -0,0 +1,22 @@
+$(function(){
+  mergecell('day-col');
+  mergecell('time-col');
+});
+
+
+function mergecell(colClass){
+  var topMatchTd;
+  var previousValue = "";
+  var rowSpan = 1;
+  $('.'+colClass).each(function(){
+     if($(this).text() == previousValue){
+       rowSpan++;
+       $(topMatchTd).attr('rowspan',rowSpan);
+       $(this).remove();
+     } else {
+       topMatchTd = $(this);
+       rowSpan = 1;
+     }
+     previousValue = $(this).text();
+  });
+}
