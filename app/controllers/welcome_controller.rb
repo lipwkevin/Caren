67425af4ce23bb6a1a6793a0d5f6288c296833f2
Kevin Li <lipwkevin@gmail.com>
2017-05-25 14:56:28 -0700

    add overview in homepage

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,16 @@
 class WelcomeController < ApplicationController
   def home
+    if current_user
+      date = Date.today
+      startDate = date
+      endDate = date+13
+      @date_range = []
+      (startDate..endDate).map{|date| @date_range.push({
+        date: date,
+        active: (date.month == date.month),
+        events: current_user.get_schedule_with_filter(date,session[:filters])
+      })}
+    end
   end
 
   def about
